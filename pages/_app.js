7a48286d99b425d628434f693388a973c1d56468
reza <syahreza.fauzi@rsmurniteguh.com>
2021-10-09 22:17:06 +0700

    server

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
+import { Provider } from 'react-redux'
 import '../styles/globals.css'
+// import store from './store'
 
 function MyApp({ Component, pageProps }) {
+  console.log('imehereeee2')
   return <Component {...pageProps} />
+}
+
+function getInitialProps(){
+  console.log('imehereeee')
 }
 
 export default MyApp
